928276c76fd203f4ef4fb9aed0fd912fbc006ff1
Artyom <artemshimon2000@gmail.com>
2020-10-17 23:21:03 +0300

    test 1-1 completed

---
+++ src/Java_tests/Test_1_1.java
@@ -0,0 +1,37 @@
+package Java_tests;
+
+import java.sql.SQLOutput;
+
+public class Test_1_1 {
+    public static void main(String[] args) {
+        int n = 10;
+        double[] a = new double[n];
+        for (int i=0;i<n;i++){
+            a[i] = (int)(Math.random()*1000);
+        }
+        double min = a[0];
+        double max = a[0];
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i]<min){
+                min = a[i];
+            }
+            if (a[i]>max){
+                max = a[i];
+            }
+            sum +=a[i];
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(a[i]);
+        }
+        System.out.println();
+        double aver = sum/n;
+        double aver_num = a[0];
+        for (int i = 1; i < n; i++) {
+            if (Math.abs(a[i]-aver) < Math.abs(aver_num-aver)){
+                aver_num = a[i];
+            }
+        }
+        System.out.println(min+"\n"+max+"\n"+aver_num+"\n"+aver);
+    }
+}
